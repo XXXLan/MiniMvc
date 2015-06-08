66cbab5f922dbb305a1da50d7b85fa3108815b42
Durian PC <lanchao@outlook.com>
2015-06-08 20:44:38 +0800

    new add code

---
+++ src/MiniMVC/MvcHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MiniMVC
+{
+    public class MvcHandler:IHttpHandler
+    {
+        public bool IsReusable
+        {
+            get { return false; }
+        }
+
+        public RequestContext RequerstContext { get; private set; }
+
+        public MvcHandler(RequestContext requestContext)
+        {
+            this.RequerstContext = requestContext;
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            string controllerName = this.RequerstContext.RouteData.Controller;
+            
+        }
+    }
+}
